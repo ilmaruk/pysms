bfd50173fec8a3d9be1af0e4a48e640c496f0049
ilmaruk <ruggero.ferretti@gmail.com>
2022-09-23 15:59:18 +0200

    Coverage report

--- setup.py
+++ setup.py
@@ -6,8 +6,16 @@ setup(
     version="0.0.0",
     packages=find_packages(),
     install_requires=[
-        "pytest==7.1.3"
+        "pydantic==1.10.2"
     ],
+    extras_require={
+        "'dev'": [
+            "autopep8==1.7.0",
+            "flake8==5.0.4",
+            "pytest==7.1.3",
+            "pytest-cov==3.0.0"
+        ]
+    },
     entry_points="""
     [console_scripts]
     pysms=pysms.pysms:pysms
