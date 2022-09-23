31d938aa8a7216f5b2e1230fbe59c34737231373
ilmaruk <ruggero.ferretti@gmail.com>
2022-09-23 15:35:23 +0200

    Test github action.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="PySMS",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "pytest==7.1.3"
+    ]
+)
