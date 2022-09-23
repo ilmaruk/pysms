756dcf68a93cd9ff9ae77fa54fe54cd85958a16e
ilmaruk <ruggero.ferretti@gmail.com>
2022-09-23 15:46:07 +0200

    Basic entry points.

---
+++ pysms/pysms.py
@@ -0,0 +1,6 @@
+def pysms():
+    print("PySMS: Main engine")
+
+
+def tsc():
+    print("PySMS: Teamscheet creator")
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from importlib.metadata import entry_points
 from setuptools import find_packages, setup
 
 setup(
@@ -6,5 +7,10 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pytest==7.1.3"
-    ]
+    ],
+    entry_points="""
+    [console_scripts]
+    pysms=pysms.pysms:pysms
+    tsc=pysms.pysms:tsc
+    """
 )
