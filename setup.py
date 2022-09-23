1d7fe65b98eed527e54252b63ced3d9c6c08aa14
ilmaruk <ruggero.ferretti@gmail.com>
2022-09-23 17:35:26 +0200

    Temporary.

--- setup.py
+++ setup.py
@@ -7,15 +7,17 @@ setup(
     packages=find_packages(),
     install_requires=[
         "pydantic==1.10.2"
+        "pytest==7.1.3",
+        "pytest-cov==3.0.0"
     ],
-    extras_require={
-        "'dev'": [
-            "autopep8==1.7.0",
-            "flake8==5.0.4",
-            "pytest==7.1.3",
-            "pytest-cov==3.0.0"
-        ]
-    },
+    # extras_require={
+    #     "'dev'": [
+    #         "autopep8==1.7.0",
+    #         "flake8==5.0.4",
+    #         "pytest==7.1.3",
+    #         "pytest-cov==3.0.0"
+    #     ]
+    # },
     entry_points="""
     [console_scripts]
     pysms=pysms.pysms:pysms
