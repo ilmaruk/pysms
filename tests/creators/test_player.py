f66d8a79eb6853a6e96f97db5bf3cf078908968f
Ruggero Ferretti <ruggero.ferretti@audioanalytic.com>
2022-09-28 11:14:39 +0200

    Tempoirarily fixing tests

--- tests/creators/test_player.py
+++ tests/creators/test_player.py
@@ -1,8 +1,9 @@
-from pysms.creators import create_player
-from pysms.models import Player
+from pysms.creators import create_player, PlayerCreateConfig
+from pysms.models import Player, PlayerPosition
 
 
 def test_create_player():
-    player = create_player()
+    player = create_player(PlayerCreateConfig(), PlayerPosition.GOALKEEPER)
     print(player.json())
     assert type(player) == Player
+    assert player.position == PlayerPosition.GOALKEEPER
